550a43ef1d77b1123fa6603b06bd0973b5f949ca
immisius <immisius@gmail.com>
2023-04-13 12:45:07 +0900

    logo add bottuns

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -15,7 +15,7 @@ importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
 );
 
-importScripts("/precache-manifest.b64f8195f160b3a9d3bcb18e10694e82.js");
+importScripts("/precache-manifest.0d44477ae477dba6dafc1786e1d16d50.js");
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
